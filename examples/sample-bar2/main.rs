2909e1fc667fcfddc6df77a4f93ee524773f50b2
Hao Hou <haohou302@gmail.com>
2019-01-11 18:58:12 -0700

    Add more exmaples

---
+++ examples/sample-bar2/main.rs
@@ -0,0 +1,29 @@
+use i3monkit::{I3Protocol, Header, WidgetCollection};
+use i3monkit::widgets::*;
+
+fn main() {
+    let p = I3Protocol::new(Header::new(1), std::io::stdout());
+
+    let mut bar = WidgetCollection::new();
+
+    let client = StockClient::new("alpha-vantage-api-key");
+    bar.push(StockClient::create_widget(&client, "MSFT"));
+    bar.push(StockClient::create_widget(&client, "AMD"));
+    bar.push(StockClient::create_widget(&client, "FB"));
+    bar.push(StockClient::create_widget(&client, "AMZN"));
+    bar.push(StockClient::create_widget(&client, "GOOG"));
+    bar.push(StockClient::create_widget(&client, "AAPL"));
+    bar.push(StockClient::create_widget(&client, "NVDA"));
+
+    bar.push(NetworkSpeedWidget::new("wlp58s0"));
+
+    for i in 0..4 {
+        bar.push(CpuWidget::new(i));
+    }
+    
+    bar.push(VolumeWidget::new("default", "Master", 0));
+    bar.push(BatteryWidget::new(0));
+    bar.push(DateTimeWidget::new());
+
+    bar.update_loop(p);
+}
